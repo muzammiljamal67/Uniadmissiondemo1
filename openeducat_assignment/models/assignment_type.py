c7baff879725c220857e7b3693c263d78aa91c4c
muzammiljamal67 <74593093+muzammiljamal67@users.noreply.github.com>
2020-12-07 16:39:45 +0530

    [ADD] app openeducat_erp

---
+++ openeducat_assignment/models/assignment_type.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Part of OpenEduCat. See LICENSE file for full copyright & licensing details.
+
+##############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2009-TODAY Tech Receptives(<http://www.techreceptives.com>).
+#
+##############################################################################
+
+from odoo import models, fields
+
+
+class GradingAssigmentType(models.Model):
+    _name = 'grading.assignment.type'
+    _description = "Assignment Type"
+
+    name = fields.Char(string="Name", required=True)
+    code = fields.Char(string="Code")
+    assign_type = fields.Selection([('sub', 'Subjective'),
+                                    ('attendance', 'Attendance')],
+                                   string='Type', default='sub')
+    calculation_method = fields.Selection([('actual', 'Actual'),
+                                           ('best_score', 'Best Score'),
+                                           ('lowest_score', 'Lowest Score'),
+                                           ('average', 'Average')],
+                                          string='Calculation Methhod',
+                                          default='actual', required=True)
